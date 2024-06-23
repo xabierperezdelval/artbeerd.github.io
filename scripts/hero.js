473ae3dbfc1035a94b9293aaa3f411dad855faa2
Xabier Pérez Del Val <xabierperezdelval@gmail.com>
2024-06-23 17:19:25 +0200

    added tabs and content

---
+++ scripts/hero.js
@@ -0,0 +1,28 @@
+function displayRandomMessage() {
+  var subtitlesData = [
+    "No, YOU are the alcoholic!",
+    "Nevermind the beer, here for the achievements.",
+    "'Uma cerveja por favor' means 'You're hot AF' in Portuguese",
+    "The only thing sour in my life is beer. Fruity, preferably.",
+    "Rating wisely after the fifth beer, now THAT'S being a hero!",
+    "More bitter than your ex's heart.",
+    "Hangovers gamified.",
+    "The darker your beer, the harder you fall.",
+    "Did you know? A single pastry stout pint counts as a full daily calorie intake!",
+    "Pretend you know about beer, it's actually fun!",
+    "Sticks and stones may break my bones, but barleywines also hurt me.",
+    "I got 99 problems but a DIPA ain't one",
+    "Day 42: The barleywine isn't RAISIN any suspicion",
+    "Spinosaurus Aegyptiacus wouldn't want this warm piss for me to take",
+    "One NEIPA aaaaand you've tasted 'em all"
+  ];
+
+  var randomIndex = Math.floor(Math.random() * subtitlesData.length);
+  var randomMessage = subtitlesData[randomIndex];
+
+  var messageElement = document.getElementById("heroSubtitle");
+  messageElement.innerHTML = randomMessage;
+}
+
+// Call the function when the page loads
+window.onload = displayRandomMessage;
