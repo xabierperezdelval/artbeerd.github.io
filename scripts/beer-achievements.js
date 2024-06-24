0d6c23ab0a85274abbf2adce1cdf74c3aa731c42
Xabier Pérez Del Val <xabierperezdelval@gmail.com>
2024-06-24 16:55:59 +0200

    feat: improve layout and add event logic

--- scripts/beer-achievements.js
+++ scripts/beer-achievements.js
@@ -1,3 +1,13 @@
-function addAchievement1Value(event, item){
-    debugger;
+function addSimpleProgressAchievementValue(event, item){
+    const currentAchiev = document.querySelector(`#${item.parentNode.parentNode.id}`);
+    let progressBar = document.querySelector(`#${currentAchiev.id} progress`);
+    progressBar.value += 1;
+}
+
+function addMultipleButtonProgressAchievementValue(event, item){
+    const currentAchiev = document.querySelector(`#${item.parentNode.parentNode.id}`);
+    let currentButton = document.querySelector(`#${item.id}`);
+    currentButton.setAttribute('disabled', true);
+    let progressBar = document.querySelector(`#${currentAchiev.id} progress`);
+    progressBar.value += 1;
 }
