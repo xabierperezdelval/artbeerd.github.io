a335bc4f86748ae52936e0916ccf02d8d0ed69de
Xabier Pérez Del Val <xabierperezdelval@gmail.com>
2024-06-25 17:00:14 +0200

    feat: modify layout; add level and disabled logic

--- scripts/beer-achievements.js
+++ scripts/beer-achievements.js
@@ -2,6 +2,9 @@ function addSimpleProgressAchievementValue(event, item){
     const currentAchiev = document.querySelector(`#${item.parentNode.parentNode.id}`);
     let progressBar = document.querySelector(`#${currentAchiev.id} progress`);
     progressBar.value += 1;
+    if (progressBar.value >= progressBar.max) {
+        updateAchievementLevel(currentAchiev);
+    }
 }
 
 function addMultipleButtonProgressAchievementValue(event, item){
@@ -10,4 +13,17 @@ function addMultipleButtonProgressAchievementValue(event, item){
     currentButton.setAttribute('disabled', true);
     let progressBar = document.querySelector(`#${currentAchiev.id} progress`);
     progressBar.value += 1;
+    if (progressBar.value >= progressBar.max) {
+        updateAchievementLevel(currentAchiev);
+    }
+}
+
+function updateAchievementLevel(currentAchiev) {
+    let progressBar = document.querySelector(`#${currentAchiev.id} progress`);
+    let levelTitle = document.querySelector(`#${currentAchiev.id} .achievement-level-title`);
+    let icon = document.querySelector(`#${currentAchiev.id} i`);
+    progressBar.value = 0;
+    levelTitle.innerHTML = "Level 1";
+    levelTitle.classList.remove('is-disabled');
+    icon.classList.remove('is-disabled');
 }
